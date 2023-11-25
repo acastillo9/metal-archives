7088ab6e43ff0fc8497198fa13cf50094144df3d
Andrés Castillo <info@acastillo.dev>
2023-11-25 18:16:25 -0500

    feat: add band endpoint

---
+++ src/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const jsdom = require("jsdom");
+
+const app = express();
+const port = 3000;
+const { JSDOM } = jsdom;
+
+app.get("/band/:bandName", async (req, res) => {
+  const { bandName } = req.params;
+  const {
+    window: { document },
+  } = await JSDOM.fromURL(`https://www.metal-archives.com/bands/${bandName}`);
+  const band = {
+    bandName: document.querySelector("#band_info > h1 > a").textContent,
+    logo: document.querySelector("#logo > img").getAttribute("src"),
+    bandImage: document.querySelector("#photo > img").getAttribute("src"),
+  };
+
+  res.send(band);
+});
+
+app.listen(port, () => {
+  console.log(`Metal Archives App listening on port ${port}`);
+});
