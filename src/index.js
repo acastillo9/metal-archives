ca81ae47e91710f8351c464dfec9058c5919ff1b
Andrés Castillo <info@acastillo.dev>
2023-11-26 17:44:15 -0500

    fix: solve error searching for a duplicated band name

--- src/index.js
+++ src/index.js
@@ -13,16 +13,56 @@ app.get("/bands/:bandName", async (req, res) => {
     window: { document },
   } = await JSDOM.fromURL(`${BASE_URL}/bands/${bandName}`);
 
+  const bands = [];
+  const bandList = document.querySelectorAll("#content_wrapper > ul > li > a");
+  if (bandList) {
+    for (const bandItem of bandList) {
+      const url = bandItem.getAttribute("href");
+      const band = await getBandFromURL(url);
+      bands.push(band);
+    }
+  } else {
+    const band = await getBand(document);
+    bands.push(band);
+  }
+
+  res.send(bands);
+});
+
+app.listen(port, () => {
+  console.log(`Metal Archives App listening on port ${port}`);
+});
+
+async function getBandFromURL(url) {
+  const {
+    window: { document },
+  } = await JSDOM.fromURL(url);
+
+  return getBand(document);
+}
+
+async function getBand(document) {
   // Comment
   const comment = await getComment(document);
 
   // Discography
   const discography = await getDiscography(document);
 
+  // label
+  let labelElement = document.querySelector(
+    "#band_stats > dl.float_right > dd:nth-child(6) > a",
+  );
+  if (!labelElement) {
+    labelElement = document.querySelector(
+      "#band_stats > dl.float_right > dd:nth-child(6)",
+    );
+  }
+  const label = labelElement.textContent;
+
   const band = {
     bandName: document.querySelector("#band_info > h1 > a").textContent,
-    logo: document.querySelector("#logo > img").getAttribute("src"),
-    bandImage: document.querySelector("#photo > img").getAttribute("src"),
+    logo: document.querySelector("#logo > img")?.getAttribute("src"),
+    bandImage: document.querySelector("#photo > img")?.getAttribute("src"),
     comment,
     countryOfOrigin: document.querySelector(
       "#band_stats > dl.float_left > dd:nth-child(2) > a",
@@ -44,32 +84,23 @@ app.get("/bands/:bandName", async (req, res) => {
     themes: document.querySelector(
       "#band_stats > dl.float_right > dd:nth-child(4)",
     ).textContent,
-    lastLabel: document.querySelector(
-      "#band_stats > dl.float_right > dd:nth-child(6) > a",
-    ).textContent,
+    label,
     discography,
   };
-
-  res.send(band);
-});
-
-app.listen(port, () => {
-  console.log(`Metal Archives App listening on port ${port}`);
-});
+  return band;
+}
 
 async function getComment(document) {
   let comment;
   const commentSeeMore = document.querySelector(
-    "#band_info > div.band_comment.clear > div > a",
+    "#band_info > div.band_comment .btn_read_more",
   );
   if (commentSeeMore) {
     const path = extractCommentURL(commentSeeMore.getAttribute("onclick"));
     const response = await fetch(`${BASE_URL}/${path}`);
     comment = await response.text();
   } else {
-    comment = document.querySelector(
-      "#band_info > div.band_comment.clear",
-    ).innerHTML;
+    comment = document.querySelector("#band_info > div.band_comment").innerHTML;
   }
   return comment;
 }
@@ -99,12 +130,14 @@ async function getDiscography(document) {
   const discography = [];
   tableRows.forEach((row) => {
     let cells = row.querySelectorAll("td");
-    let album = {
-      name: cells[0].textContent.trim(),
-      type: cells[1].textContent.trim(),
-      year: cells[2].textContent.trim(),
-    };
-    discography.push(album);
+    if (!cells[0].querySelector("em")) {
+      let album = {
+        name: cells[0].textContent.trim(),
+        type: cells[1].textContent.trim(),
+        year: cells[2].textContent.trim(),
+      };
+      discography.push(album);
+    }
   });
   return discography;
 }
